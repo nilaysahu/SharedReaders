6696058bcf90d6c71260da54e495f2dcbde1f916
nilay sahu <sahu.nilay@gmail.com>
2015-08-16 22:57:41 +0530

    Adding BACKEND code - SharedReaderBackbone

---
+++ SharedReaderbackbone/src/main/java/com/example/nisahu/myapplication/backend/HttpRequestHandler.java
@@ -0,0 +1,34 @@
+package com.example.nisahu.myapplication.backend;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by nisahu on 16/08/2015.
+ */
+public class HttpRequestHandler {
+
+    public  String Perform(String query) {
+        try {
+            URL url = new URL("https://www.goodreads.com/search.xml?key=JvO2RkMBST74HI7z0famA&q="+query);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+
+            return e.toString();
+        }
+    }
+}
---
+++ SharedReaderbackbone/src/main/java/com/example/nisahu/myapplication/backend/MyEndpoint.java
@@ -0,0 +1,50 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.example.nisahu.myapplication.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import java.net.URL;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.nisahu.example.com",
+                ownerName = "backend.myapplication.nisahu.example.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+    @ApiMethod(name="GetBookListByQuery")
+    public MyBean GetBookListByQuery(@Named("name") String name)
+    {
+        MyBean response = new MyBean();
+        HttpRequestHandler request = new HttpRequestHandler();
+        response.setData(request.Perform(name));
+        return response;
+    }
+
+}
